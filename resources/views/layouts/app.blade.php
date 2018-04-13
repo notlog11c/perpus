e2607a08ecc7fb88e8fa6cb0590f890fd8a04cd8
Rifky <notlog11c>
2018-04-13 09:39:01 +0700

    ins laratrust

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'LibraryO') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -22,7 +22,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/fontawesome-all.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/dadadu.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/dadadu.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
