b6bc025b3774ffd64386bdf8eb00763e7a59b362
Rifky <notlog11c>
2018-04-18 13:53:32 +0700

    penggunaaan modal event

--- app/Author.php
+++ app/Author.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Session;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
@@ -11,5 +12,32 @@ class Author extends Model
     public function books()
     {
         return $this->hasMany(Book::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        
+        self::deleting(function ($author){
+
+        
+            if ($author->books->count() > 0) {
+                # code...
+                $messageHtml = 'penulis tidak bisa dihapus karena masih memiliki buku!';
+                $messageHtml .= '<ul>';
+                foreach ($author->books as $book) {
+                    $messageHtml .= "<li>$book->title</li>";
+                }
+
+                $messageHtml .= '<ul>';
+
+                Session::flash('flash_notification', [
+                    'level' => 'danger',
+                    'message' => $messageHtml,
+                ]);
+
+                return false;
+            }
+        });
     }
 }
